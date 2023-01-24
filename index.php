7e18dbb7ed1523b2d8ffa22f1d36401fd8ed1e06
matt-j-345 <120670341+matt-j-345@users.noreply.github.com>
2023-01-24 14:48:20 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
 <?php
 session_start();
-$SQLquery = $data = " ";
+if (empty($_SESSION['token'])) {
+    $_SESSION['token'] = bin2hex(random_bytes(32));
+}
+
+error_reporting(0);
+
+ini_set('session.cookie_samesite', "strict");
+ini_set('session.cookie_httponly', 1);
+/*
+        session_set_cookie_params([
+            'lifetime' => 3600,
+            'path' => '/',
+            'domain' => $_SERVER['HTTP_HOST'],
+            'secure' => True,
+            'httponly' => True,
+            'samesite' => 'strict'
+        ]);
+*/
+
+$SQLquery = $data = " ";																			// fixing csrf risk
 
 if (isset($_POST['login'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+        $username = htmlentities($_POST['username'], ENT_QUOTES, 'UTF-8');												// fixed this xss, sqlinjection
+		$password = htmlentities($password = $_POST['password'], ENT_QUOTES, 'UTF-8');									// fixed this xss, sqlinjection
+        
 		
 		$conn = new mysqli("localhost", "root", "", "softwaresecurity");
-		
 		$SQLquery = "SELECT id from users where username = '" . $username . "' and password = '" . $password . "'";
 		
 		$result = $conn->query($SQLquery);
@@ -16,6 +35,8 @@ if (isset($_POST['login'])) {
 		$result->close();
 		$conn->close();	
 }
+
+echo "anti CSRF Token: " . $_SESSION['token'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -24,13 +45,15 @@ if (isset($_POST['login'])) {
   </head>
   <body>
 	<form method="post" action="" name="signin-form">
+	<input type="hidden" name="CSRFToken" value=<?php $_SESSION['token'] ?>>											<!-- token for anti csrf -->
+	
 	  <div class="form-element">
 		<label>Username</label>
-		<input type="text" name="username"  />
+		<input type="text" name="username" required />     																<!-- fixed for parameter tampering -->
 	  </div>
       <div class="form-element">
 		<label>Password</label>
-		<input type="password" name="password"  />
+		<input type="text" name="password" required />																	<!-- fixed for parameter tampering -->
 	  </div>
 		<button type="submit" name="login" value="login">Log In</button>
 	</form>
